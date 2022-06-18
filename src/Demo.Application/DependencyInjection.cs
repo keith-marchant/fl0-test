3572d344af4230c02beb6ee222fe7e8d2de17343
Keith Marchant <kemarchant@outlook.com>
2022-06-18 22:43:40 +1000

    Wire up mediatr

--- src/Demo.Application/DependencyInjection.cs
+++ src/Demo.Application/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Demo.Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Demo.Application
 {
@@ -6,9 +10,38 @@ namespace Demo.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddAutoMapper(new Assembly[] { Assembly.GetExecutingAssembly() });
+            services.AddMediatR(new Assembly[] { Assembly.GetExecutingAssembly() });
+            services.AddFluentValidation(Assembly.GetExecutingAssembly());
 
-
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehaviour<,>));
             return services;
+        }
+
+        private static void AddFluentValidation(this IServiceCollection services, Assembly assembly)
+        {
+            var validatorType = typeof(IValidator<>);
+
+            var validatorTypes = assembly
+                .GetExportedTypes()
+                .Where(t => t.GetInterfaces().Any(i =>
+                    i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == validatorType))
+                .ToList();
+
+            foreach (var validator in validatorTypes)
+            {
+                var requestType = validator.GetInterfaces()
+                    .Where(i => i.IsGenericType &&
+                        i.GetGenericTypeDefinition() == typeof(IValidator<>))
+                    .Select(i => i.GetGenericArguments()[0])
+                    .First();
+
+                var validatorInterface = validatorType
+                    .MakeGenericType(requestType);
+
+                services.AddTransient(validatorInterface, validator);
+            }
         }
     }
 }
