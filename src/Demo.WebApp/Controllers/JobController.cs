d28e3e98d7f1d392af2872c2f95b693f9802043f
Keith Marchant <kemarchant@outlook.com>
2022-06-18 22:52:41 +1000

    Add empty controllers

---
+++ src/Demo.WebApp/Controllers/JobController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.WebApp.Controllers
+{
+    public class JobController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
